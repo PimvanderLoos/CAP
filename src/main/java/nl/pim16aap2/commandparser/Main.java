bd1662fe8f32ce4362b81ac32f450bad6fd375a0
Pim van der Loos <pimvanderloos@gmail.com>
2020-10-24 12:05:36 +0200

    Update TODO list

--- src/main/java/nl/pim16aap2/commandparser/Main.java
+++ src/main/java/nl/pim16aap2/commandparser/Main.java
@@ -22,39 +22,25 @@ import java.io.EOFException;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO: Consider using ByteBuddy to generate the commands? Might be completely overkill, though
 // TODO: Add some way to inject text into the Text system. For example, when adding a piece of text with
 //       a command, store which command it is and if it is listed as a subcommand or a supercommand.
 //       Allow registering event handlers or something when a new component is added.
 //       This is useful for stuff like clickable text. In Minecraft, this would be the ability to click on a command
 //       in a help menu to execute the help command for that command.
-// TODO: Allow positional optional arguments. Perhaps Required and OptionalPositional can both extend a Positional interface?
-//       Maybe also extend a FlaggedArgument interface for stuff that does require flags?
-//       This can be useful for stuff like "/bigdoors help addowner".
 // TODO: Allow using space as a separator of flag-value pairs.
 // TODO: Make a class somewhere where you can register ColorScheme objects. This class can then be used for caching
 //       stuff like finished Texts etc.
 //       For this to work properly, there should be some mechanism to keep track of whether a command(system) was
 //       changed since it was last cached. For example, if subcommands are added, the cache will have to be invalidated.
-// TODO: For the HelpCommand, just make it a boolean for the command (default true) to always look for it in the format
-//       "/whatever command help [arguments]". Provide a default HelpCommand thingy, but also make it a builder,
-//       to fully customize how the help command works. For example, hidden commands should probably display their help
-//       also when no arguments are provided at all? Also, make the default help command extend an IHelpCommand interface
-//       so you can just provide your own custom one if the options aren't sufficient.
+// TODO: Hidden commands should have a default executor that just displays the help menu.
 // TODO: Add permissions to commands (and arguments?). Probably a setter via an interface.
 // TODO: Command/argument(name/value) completion.
-// TODO: Subcommand sorting + sorting options (alphabetical? Length? Creation order?).
-//       Currently, it's a List, but perhaps a LinkedHashMap would be better for this?
-//       https://docs.oracle.com/javase/10/docs/api/java/util/LinkedHashMap.html
-//       also, arguments should use this as well, though those should not be sortable; they should just be sorted based
-//       on insertion order.
 // TODO: Allow defining and supplying custom renderers.
 // TODO: For the argument renderers, allow specifying long/short name requirements and stuff. In some situations,
 //       having both might be nice, while in other, it's better to only have the short version. Right?
 // TODO: Customizable bracket types for optional/required arguments.
 // TODO: Make is possible to have certain commands be server-only or player-only. These commands shouldn't show up in
 //       the help menus.
-// TODO: Maybe flag parameters (e.g. --admin), aka valueless parameters should have their own class?
 // TODO: Allow the use of empty lines. For Spigot (and probably other platforms?) '\n' isn't good enough.
 //       Instead, Spigot needs a color code on an otherwise empty line to have empty lines.
 //       Perhaps this can be done via the color scheme?
@@ -63,20 +49,15 @@ import java.util.List;
 // TODO: Do not use 'helpful' messages in exceptions, but just variables. Whomever catches the exception
 //       Should be able to easily parse it themselves. If an exception requires additional text to explain it
 //       then it's time to create a new type or at the very least a new constructor.
-// TODO: Every command needs to be able to print its own help message. Maybe every command should have its own usage
-//       renderer? The help renderer can just use that to render the complete help command.
 // TODO: Make it possible to use functions to verify arguments. E.g. for IntArgument: Arg must be in [1, 10].
 // TODO: Make a module specific for Spigot and/or Paper. This should contain all the stuff needed for thost platforms.
 //       E.g. BukkitCommandSender, ChatColor, Player/World Arguments, etc.
 //       For the BukkitChatColor, the builder can just accept ChatColors and take care of the default off value /
 //       instantiating TextComponents.
 // TODO: Should Optional arguments be wrapped inside Optional as well? Might be nice.
-// TODO: Store the HelpArgument in a command separately, so that we can
-//       actually figure out which argument is the help argument.
 // TODO: Unit tests.
 // TODO: Make sure that positional arguments fed in the wrong order gets handled gracefully
 //       (there are probably going to be some casting issues).
-// TODO: ChatColors for required labels/separators/etc, now that free required arguments are possible.
 // TODO: Maybe store the arguments by their label inside the CommandResult? That would avoid confusion of name vs longName.
 // TODO: Be more consistent in naming help menus. There should be a clear distinction between the command-specific long help
 //       and the command's list of subcommands. Maybe help text and help menu?
@@ -101,7 +82,7 @@ public class Main
         final String command = arrToString(args);
         System.out.println(command + ":\n");
         final DefaultCommandSender commandSender = new DefaultCommandSender();
-        commandSender.setColorScheme(Main.getColorScheme());
+//        commandSender.setColorScheme(Main.getColorScheme());
 
         try
         {
