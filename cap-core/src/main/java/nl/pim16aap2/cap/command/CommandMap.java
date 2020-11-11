b757b980faefa8e00469fecbe0626d3e8f5f4701
Pim van der Loos <pimvanderloos@gmail.com>
2020-11-11 19:47:20 +0100

    Made CommandMap more generic
    - Extracted a lot of code out of the CommandMap class into the new LocalizedMap class (of which the CommandMap class is now a subclass). This makes it possible to reuse the same code for the argument maps.

--- cap-core/src/main/java/nl/pim16aap2/cap/command/CommandMap.java
+++ cap-core/src/main/java/nl/pim16aap2/cap/command/CommandMap.java
@@ -2,33 +2,22 @@ package nl.pim16aap2.cap.command;
 
 import lombok.NonNull;
 import nl.pim16aap2.cap.CAP;
-import nl.pim16aap2.cap.util.Util;
+import nl.pim16aap2.cap.util.LocalizedMap;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Optional;
 
-public class CommandMap
+/**
+ * The {@link LocalizedMap} for {@link Command}s.
+ *
+ * @author Pim
+ */
+public class CommandMap extends LocalizedMap<Command>
 {
-    /**
-     * The {@link CAP} instance managing this object.
-     */
-    private final @NonNull CAP cap;
-
-    /**
-     * The map containing all registered commands, with their names as key.
-     */
-    protected final @NonNull Map<@NonNull Locale, Map<@NonNull String, @NonNull Command>> commandMap = new LinkedHashMap<>();
-
     public CommandMap(final @NonNull CAP cap)
     {
-        this.cap = cap;
-
-        for (final @NonNull Locale locale : cap.getLocales())
-            commandMap.put(locale, new HashMap<>());
+        super(cap);
     }
 
     /**
@@ -40,14 +29,7 @@ public class CommandMap
      */
     public @NonNull Optional<Command> getCommand(@Nullable String name, @Nullable Locale locale)
     {
-        locale = Util.valOrDefault(locale, cap.getDefaultLocale());
-        if (name == null)
-            return Optional.empty();
-
-        name = cap.getMessage(name, locale);
-
-        @NonNull Optional<Command> cmd = getFromMap(commandMap, locale, cap.getCommandNameCaseCheck(name));
-        return cmd;
+        return getEntry(name, locale, cap::getCommandNameCaseCheck);
     }
 
     /**
@@ -57,39 +39,6 @@ public class CommandMap
      */
     public void addCommand(final @NonNull Command command)
     {
-        for (final @NonNull Locale locale : cap.getLocales())
-        {
-            final @NonNull String name = cap.getCommandNameCaseCheck(cap.getMessage(command.getName(), locale));
-            commandMap.get(locale).put(name, command);
-        }
-    }
-
-    /**
-     * Gets an entry from a localized map.
-     *
-     * @param map    The {@link Map} to retrieve the entry from.
-     * @param locale The {@link Locale} to search in.
-     * @param key    The key to search for in the locale.
-     * @param <T>    The type of the value to search for.
-     * @return The value if it could be found.
-     */
-    private @NonNull <T> Optional<T> getFromMap(
-        final @NonNull Map<@NonNull Locale, Map<@NonNull String, @NonNull T>> map,
-        final Locale locale, final @NonNull String key)
-    {
-        return Optional.ofNullable(map.get(locale)).map(entry -> entry.get(key));
-    }
-
-    public @NonNull Map<@NonNull String, @NonNull Command> get()
-    {
-        return get(null);
-    }
-
-    public @NonNull Map<@NonNull String, @NonNull Command> get(final @Nullable Locale locale)
-    {
-        final @NonNull Map<@NonNull String, @NonNull Command> map = commandMap
-            .get(Util.valOrDefault(locale, cap.getDefaultLocale()));
-
-        return map;
+        addEntry(command.getName(), command, cap::getCommandNameCaseCheck);
     }
 }
---
+++ cap-core/src/main/java/nl/pim16aap2/cap/util/LocalizedMap.java
@@ -0,0 +1,105 @@
+package nl.pim16aap2.cap.util;
+
+import lombok.NonNull;
+import nl.pim16aap2.cap.CAP;
+import nl.pim16aap2.cap.command.Command;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * Represents a map of values with Strings as keys for several {@link Locale}s.
+ *
+ * @param <T> The type of the values to store.
+ * @author Pim
+ */
+public abstract class LocalizedMap<T>
+{
+    /**
+     * The {@link CAP} instance managing this object.
+     */
+    protected final @NonNull CAP cap;
+
+    /**
+     * The map containing all registered items, with their names as key.
+     */
+    private final @NonNull Map<@NonNull Locale, Map<@NonNull String, @NonNull T>> localizedMap = new LinkedHashMap<>();
+
+    protected LocalizedMap(final @NonNull CAP cap)
+    {
+        this.cap = cap;
+
+        for (final @NonNull Locale locale : cap.getLocales())
+            localizedMap.put(locale, new HashMap<>());
+    }
+
+    /**
+     * Adds the entry for every locale.
+     *
+     * @param key         The key of the entry to register to register.
+     * @param caseChecker The function to use to make sure the case is correct. (e.g. {@link
+     *                    CAP#getCommandNameCaseCheck(String)}.
+     */
+    protected void addEntry(final @NonNull String key, final @NonNull T value,
+                            final @NonNull Function<@NonNull String, @NonNull String> caseChecker)
+    {
+        for (final @NonNull Locale locale : cap.getLocales())
+        {
+            final @NonNull String name = caseChecker.apply(cap.getMessage(key, locale));
+            localizedMap.get(locale).put(name, value);
+        }
+    }
+
+    /**
+     * Gets a {@link Command} from its name.
+     *
+     * @param key         The key to search for.
+     * @param locale      The {@link Locale} for which to get the {@link Command}.
+     * @param caseChecker The function to use to make sure the case is correct. (e.g. {@link
+     *                    CAP#getCommandNameCaseCheck(String)}.
+     * @return The entry associated with the provided key in the given locale, if it could be found.
+     */
+    protected @NonNull Optional<T> getEntry(@Nullable String key, @Nullable Locale locale,
+                                            final @NonNull Function<@NonNull String, @NonNull String> caseChecker)
+    {
+        locale = Util.valOrDefault(locale, cap.getDefaultLocale());
+        if (key == null)
+            return Optional.empty();
+
+        key = cap.getMessage(key, locale);
+
+        final @NonNull Optional<T> cmd = getFromMap(localizedMap, locale, caseChecker.apply(key));
+        return cmd;
+    }
+
+    /**
+     * Gets an entry from a localized map.
+     *
+     * @param map    The {@link Map} to retrieve the entry from.
+     * @param locale The {@link Locale} to search in.
+     * @param key    The key to search for in the locale.
+     * @return The value if it could be found.
+     */
+    private @NonNull Optional<T> getFromMap(final @NonNull Map<@NonNull Locale, Map<@NonNull String, @NonNull T>> map,
+                                            final Locale locale, final @NonNull String key)
+    {
+        return Optional.ofNullable(map.get(locale)).map(entry -> entry.get(key));
+    }
+
+    public @NonNull Map<@NonNull String, @NonNull T> get()
+    {
+        return get(null);
+    }
+
+    public @NonNull Map<@NonNull String, @NonNull T> get(final @Nullable Locale locale)
+    {
+        final @NonNull Map<@NonNull String, @NonNull T> map = localizedMap.get(Util.valOrDefault(locale,
+                                                                                                 cap.getDefaultLocale()));
+        return map;
+    }
+}
